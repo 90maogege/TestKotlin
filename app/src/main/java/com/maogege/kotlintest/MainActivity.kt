d2d9fd63125c3faa9678d803f97df243bdbfe94e
90maogege <929895321@qq.com>
2021-06-15 15:23:44 +0800

    Update MainActivity.kt

--- app/src/main/java/com/maogege/kotlintest/MainActivity.kt
+++ app/src/main/java/com/maogege/kotlintest/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 /**
- * 测试
+ * 测试  ---
  */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
